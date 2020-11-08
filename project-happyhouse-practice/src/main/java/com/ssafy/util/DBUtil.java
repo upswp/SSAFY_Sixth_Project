b40079e583c9e3a04e43ebba16e32611fcab2625
박상우 <70404643+upswp@users.noreply.github.com>
2020-11-08 20:17:15 +0900

    util 업로드 1차

---
+++ project-happyhouse-practice/src/main/java/com/ssafy/util/DBUtil.java
@@ -0,0 +1,12 @@
+package com.ssafy.util;
+public class DBUtil {
+	public static void close(AutoCloseable c) {
+		if (c != null) {
+			try {
+				c.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
